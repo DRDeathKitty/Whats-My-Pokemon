c476faef7f10930dd11e39b88987eaca840778cf
Skyler Stark <34877759+DRDeathKitty@users.noreply.github.com>
2019-04-22 13:17:45 -0400

    Add files via upload

---
+++ Whats My Pokemon/frmPokemon.Designer.vb
@@ -0,0 +1,253 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPokemon
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmPokemon))
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblAge = New System.Windows.Forms.Label()
+        Me.picPokemon = New System.Windows.Forms.PictureBox()
+        Me.lblMonthBorn = New System.Windows.Forms.Label()
+        Me.lblDayBorn = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.lblPokemon = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblPokeDex = New System.Windows.Forms.Label()
+        Me.btnWeb = New System.Windows.Forms.Button()
+        Me.txtDayBorn = New System.Windows.Forms.TextBox()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.txtMonthBorn = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblSigniture = New System.Windows.Forms.Label()
+        CType(Me.picPokemon, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(11, 3)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(105, 25)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblAge
+        '
+        Me.lblAge.AutoSize = True
+        Me.lblAge.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAge.Location = New System.Drawing.Point(44, 20)
+        Me.lblAge.Name = "lblAge"
+        Me.lblAge.Size = New System.Drawing.Size(36, 16)
+        Me.lblAge.TabIndex = 1
+        Me.lblAge.Text = "Age"
+        '
+        'picPokemon
+        '
+        Me.picPokemon.BackColor = System.Drawing.Color.Transparent
+        Me.picPokemon.Location = New System.Drawing.Point(12, 32)
+        Me.picPokemon.Name = "picPokemon"
+        Me.picPokemon.Size = New System.Drawing.Size(210, 210)
+        Me.picPokemon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picPokemon.TabIndex = 2
+        Me.picPokemon.TabStop = False
+        '
+        'lblMonthBorn
+        '
+        Me.lblMonthBorn.AutoSize = True
+        Me.lblMonthBorn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMonthBorn.Location = New System.Drawing.Point(0, 70)
+        Me.lblMonthBorn.Name = "lblMonthBorn"
+        Me.lblMonthBorn.Size = New System.Drawing.Size(85, 16)
+        Me.lblMonthBorn.TabIndex = 4
+        Me.lblMonthBorn.Text = "Month Born"
+        '
+        'lblDayBorn
+        '
+        Me.lblDayBorn.AutoSize = True
+        Me.lblDayBorn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDayBorn.Location = New System.Drawing.Point(15, 45)
+        Me.lblDayBorn.Name = "lblDayBorn"
+        Me.lblDayBorn.Size = New System.Drawing.Size(72, 16)
+        Me.lblDayBorn.TabIndex = 3
+        Me.lblDayBorn.Text = "Day Born"
+        '
+        'btnClear
+        '
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(117, 3)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(105, 25)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnRun
+        '
+        Me.btnRun.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRun.Location = New System.Drawing.Point(12, 281)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(160, 37)
+        Me.btnRun.TabIndex = 4
+        Me.btnRun.Text = "What's My Pokemon"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'lblPokemon
+        '
+        Me.lblPokemon.AutoSize = True
+        Me.lblPokemon.BackColor = System.Drawing.Color.Transparent
+        Me.lblPokemon.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPokemon.Location = New System.Drawing.Point(15, 245)
+        Me.lblPokemon.Name = "lblPokemon"
+        Me.lblPokemon.Size = New System.Drawing.Size(201, 16)
+        Me.lblPokemon.TabIndex = 7
+        Me.lblPokemon.Text = "Your Pokemon Is XXXXXXXX"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 437)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(210, 18)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "My controls are up there. ^"
+        '
+        'lblPokeDex
+        '
+        Me.lblPokeDex.AutoSize = True
+        Me.lblPokeDex.BackColor = System.Drawing.Color.Transparent
+        Me.lblPokeDex.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPokeDex.Location = New System.Drawing.Point(37, 262)
+        Me.lblPokeDex.Name = "lblPokeDex"
+        Me.lblPokeDex.Size = New System.Drawing.Size(160, 16)
+        Me.lblPokeDex.TabIndex = 10
+        Me.lblPokeDex.Text = "PokeDex Number XXX"
+        '
+        'btnWeb
+        '
+        Me.btnWeb.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnWeb.Location = New System.Drawing.Point(178, 281)
+        Me.btnWeb.Name = "btnWeb"
+        Me.btnWeb.Size = New System.Drawing.Size(44, 37)
+        Me.btnWeb.TabIndex = 5
+        Me.btnWeb.Text = "Web"
+        Me.btnWeb.UseVisualStyleBackColor = True
+        '
+        'txtDayBorn
+        '
+        Me.txtDayBorn.Location = New System.Drawing.Point(86, 44)
+        Me.txtDayBorn.Name = "txtDayBorn"
+        Me.txtDayBorn.Size = New System.Drawing.Size(118, 20)
+        Me.txtDayBorn.TabIndex = 2
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(86, 19)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(117, 20)
+        Me.txtAge.TabIndex = 1
+        '
+        'txtMonthBorn
+        '
+        Me.txtMonthBorn.Location = New System.Drawing.Point(86, 69)
+        Me.txtMonthBorn.Name = "txtMonthBorn"
+        Me.txtMonthBorn.Size = New System.Drawing.Size(117, 20)
+        Me.txtMonthBorn.TabIndex = 3
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Yellow
+        Me.GroupBox1.Controls.Add(Me.txtMonthBorn)
+        Me.GroupBox1.Controls.Add(Me.txtAge)
+        Me.GroupBox1.Controls.Add(Me.txtDayBorn)
+        Me.GroupBox1.Controls.Add(Me.lblMonthBorn)
+        Me.GroupBox1.Controls.Add(Me.lblDayBorn)
+        Me.GroupBox1.Controls.Add(Me.lblAge)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 324)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(210, 100)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        '
+        'lblSigniture
+        '
+        Me.lblSigniture.AutoSize = True
+        Me.lblSigniture.BackColor = System.Drawing.Color.Transparent
+        Me.lblSigniture.Font = New System.Drawing.Font("Stencil", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSigniture.Location = New System.Drawing.Point(21, 41)
+        Me.lblSigniture.Name = "lblSigniture"
+        Me.lblSigniture.Size = New System.Drawing.Size(192, 18)
+        Me.lblSigniture.TabIndex = 11
+        Me.lblSigniture.Text = "Made by DR.DeathKitty"
+        '
+        'frmPokemon
+        '
+        Me.AcceptButton = Me.btnRun
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Whats_My_Pokemon.My.Resources.Resources.Pokedex
+        Me.CancelButton = Me.btnClear
+        Me.ClientSize = New System.Drawing.Size(234, 436)
+        Me.Controls.Add(Me.lblSigniture)
+        Me.Controls.Add(Me.btnWeb)
+        Me.Controls.Add(Me.lblPokeDex)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblPokemon)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.picPokemon)
+        Me.Controls.Add(Me.btnExit)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(265, 513)
+        Me.Name = "frmPokemon"
+        Me.Text = "WMP"
+        CType(Me.picPokemon, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblAge As Label
+    Friend WithEvents picPokemon As PictureBox
+    Friend WithEvents lblMonthBorn As Label
+    Friend WithEvents lblDayBorn As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnRun As Button
+    Friend WithEvents lblPokemon As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblPokeDex As Label
+    Friend WithEvents btnWeb As Button
+    Friend WithEvents txtMonthBorn As TextBox
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents txtDayBorn As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lblSigniture As Label
+End Class
---
+++ Whats My Pokemon/frmPokemon.vb
@@ -0,0 +1,120 @@
+﻿' Created By DR.DeathKitty
+
+Public Class frmPokemon
+
+    ' The array of all the 649 pokemon names to use in the lblPokemon text. (This is not all the pokemon names, still missing around 150)
+    Public pokemon As New List(Of String)({"Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran", "Nidorina", "Nidoqueen", "Nidoran", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vilpix", "Ninetales", "Jigglypuff", "Wigglypuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Gravler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Ferfetche'd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Ghastly", "Haunter", "Gengar", "Onyx", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "goldeen", "Seaking", "Staryu", "Starmie", "Mr.Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapados", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew",
+            "Chikorita", "Bayleef", "Meganium", "Cyndaquill", "Quilava", "Typhlosion", "Totodile", "Croconaw", "Feraligatr", "Sentret", "Furret", "Hoothoot", "Noctowl", "Ledyba", "Ledian", "Spinarak", "Ariados", "Crobat", "Chinchou", "Lanturn", "Pichu", "Cleffa", "Igglybuff", "Togepi", "Togetic", "Natu", "Xatu", "Mareep", "Flaaffy", "Ampharos", "Bellossom", "Marill", "Azumarill", "Sudowoodo", "Politoed", "Hoppip", "Skiploom", "Jumpluff", "Aipom", "Sunkern", "Sunflora", "Yanma", "Wooper", "Quagsire", "Espeon", "Unbreon", "Murkrow", "Slowking", "Misdreavus", "Unown", "Wobuffet", "Girafarig", "Pineco", "Forretress", "Dunsparce", "Gligar", "Steelix", "Snubbull", "Granbull", "Qwilfish", "Scizor", "Shuckle", "Heracross", "Sneasel", "Teddiursa", "Ursaring", "Slugma", "Magcargo", "Swinub", "Poliswine", "Corsola", "Remoraid", "Octillery", "Delibird", "Mantine", "Skarmory", "Houndour", "Houndoom", "Kingdra", "Phanpy", "Donphan", "Porygon2", "Stantler", "Smeargle", "Tyrogue", "Hitmontop", "Smoochum", "Elekid", "Magby", "Miltank", "Blissey", "Raikou", "Entei", "Suicune", "Larvitar", "Pupitar", "Tyranitar", "Lugia", "Ho-Oh", "Celebi",
+            "Treecko", "Grocyle", "Sceptile", "Torchic", "Combusken", "Blaziken", "Mudkip", "Marshtomp", "Swampert", "Poochyena", "Mightyena", "Zigzagoon", "Linoone", "Wurmple", "Slicoon", "Beautifly", "Cascoon", "Dustoc", "Lotad", "Lonbre", "Ludicolo", "Seedot", "Nuzleaf", "Shiftry", "Taillow", "Swellow", "Wingull", "Pelipper", "Ralts", "Kirlia", "Gardevoir", "Surskit", "Masquerian", "Shroomish", "Breloom", "Slakoth", "Vigoroth", "Slaking", "Nincada", "Ninjask", "Sheninja", "Whismur", "Loudred", "Exploud", "Makuhita", "Hariyama", "Azurill", "Nosepass", "Skitty", "Delcatty", "Sableye", "Mawile", "Aron", "Lairon", "Aggron", "Meditite", "Medicham", "Electrike", "Manactric", "Plusle", "Minun", "Volbeat", "Illumise", "Roselia", "Gulpin", "Swalot", "Carvanha", "Sharpedo", "Wailmer", "Wailord", "Numel", "Camerupt", "Torkoal", "Spoink", "Grumpig", "Spinda", "Trapinch", "Vibrava", "Flygon", "Cacnea", "Cacturne", "Swablu", "Altaria", "Zangoose", "Seviper", "Lunatone", "Solrock", "Barboach", "Whiscash", "Corphish", "Crawdaunt", "Baltoy", "Claydol", "Lileep", "Cradily", "Anorith", "Armaldo", "Feebas", "Milotic", "Castform", "Kecleon", "Shuppet", "Banette", "Duskull", "Dusclops", "Torpius", "Chimecho", "Absol", "Wynaut", "Snorunt", "Glalie", "Spheal", "Sealeo", "Walrein", "Clamperl", "Hauntail", "Gorebyss", "Relicanth", "Luvdisc", "Bagon", "Shelgon", "Salamence", "Beldum", "Metang", "Metagross", "Ragirock", "Regice", "Ragisteel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Jirachi", "Deoxys",
+            "Turtwig", "Grotle", "Torterra", "Chimchar", "Monferno", "Infernape", "Piplip", "Prinplup", "Empoleon", "Starly", "Staravia", "Staraptor", "Bidoof", "Bibarel", "Kricketot", "Kricketune", "Shinx", "Luxio", "Luxray", "Budew", "Roserade", "Cranidos", "Rampardos", "Shieldon", "Bastiodon", "Burmy", "Wormadam", "Mothim", "Combee", "Vespiquen", "Pachirisu", "Buzel", "Floatzel", "Cherubi", "Cherrim", "Shellos", "Gastrodon", "Ambipom", "Drifloon", "Drifblim", "Buneary", "Lopunny", "Mismagius", "Honchkrow", "Glameow", "Purugly", "Chingling", "Stunky", "Skantank", "Bronzor", "Bronzong", "Bonsly", "Mime Jr.", "Happiny", "Chatot", "Spiritomb", "Gible", "Gabite", "Garchomp", "Munchlax", "Riolu", "Lucario", "Hippopotas", "Hippowdon", "Skorupi", "Drapion", "Croagunk", "Toxicroak", "Carnivine", "Finneon", "Lumineon", "Mantyke", "Snover", "Abomasnow", "Weavile", "Magnezone", "Lickilicky", "Rhyperior", "Tangrowth", "Electivire", "Magmortar", "Togekiss", "Yanmega", "Leafeon", "Glaceon", "Gliscor", "Mamoswine", "Porygone-Z", "Gallade", "Probopass", "Dusknoir", "Froslass", "Rotom", "Uxie", "Mesprit", "Azelf", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia", "Phione", "Manaphy", "Darkrai", "Shaymin", "Arceus",
+            "Victini", "Snivy", "Servine", "Serperior", "Tepig", "Pignite", "Emboar", "Oshawott", "DeWott", "Samurott", "Patrat", "Watchdog", "Lillipup", "Herdier", "Stoutland", "Purrloin", "Liepard", "Pansage", "Simisage", "Pansear", "Simisear", "Panpour", "Simipour", "Munna", "Musharna", "Pidove", "Tranquill", "Unfeznat", "Blitzle", "Zebstrika", "Roggenrola", "Boldore", "Gigalith", "Woobat", "Swoobat", "Drilbur", "Excadrill", "Audino", "Timburr", "Gurdurr", "Conkeldurr", "Tympole", "Palpitoad", "Seismitoad", "Throh", "Sawk", "Sewaddle", "Swadloon", "Leavanny", "Venipede", "Whirlipede", "Scolipede", "Cottonee", "Whimsicott", "Petilil", "Lilligant", "Basculin", "Sandile", "Krokorok", "Krookodile", "Darumaka", "Darmanitan", "Maractus", "Dweebble", "Crustle", "Scraggy", "Scrafty", "Sigilyph", "Yamask", "Cofagrigus", "Tirtouga", "Carracosta", "Archen", "Archeops", "Trubbish", "Garbodor", "Zorua", "Zoroark", "Minccino", "Cinccino", "Gothita", "Gotgorita", "Gothitelle", "Solosis", "Duosion", "Reuniclus", "Ducklett", "Swanna", "Vanillite", "Vanillish", "Vanilluxe", "Deerling", "Sawsbuck", "Emolga", "Karrablast", "Escavalier", "Foongus", "Amoonguss", "Frillish", "Jellicent", "Alomomola", "Joltik", "Galvantula", "Ferroseed", "Ferrothorn", "Klink", "Klang", "Klinklang", "Tynamo", "Eelektrik", "Eelektross", "Elgyem", "Beheeyem", "Litwick", "Lamnent", "Chandelure", "Axew", "Fraxure", "Haxorus", "Cubchoo", "Beartic", "Cryogonal", "Shelmet", "Accelgor", "Stunfisk", "Mienfoo", "Mienshao", "Druddigon", "Golett", "Pawniard", "Bisharp", "Bouffalant", "Rufflet", "Braviary", "Vullaby", "Mandibuzz", "Heatmor", "Durant", "Deino", "Zweilous", "Hydreigon", "Larvesta", "Volcarona", "Cobalion", "Terrakion", "Virizion", "Tornadus", "Thundurus", "Reshiram", "Zekrom", "Landorus", "Kyurem", "Keldeo", "Meloetta", "Genesect"}) ' Created By DR.DeathKitty
+    Public intAge As Integer
+    Public intDay As Integer
+    Public intMonth As Integer
+
+    
+    Public total As String
+
+    ' This disables the exit button and hides the picture and label when the program loads.
+    Private Sub frmPokemon_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnExit.Enabled = False
+        lblPokemon.Visible = False
+        picPokemon.Visible = False
+        lblPokeDex.Visible = False
+        btnWeb.Enabled = False
+    End Sub
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+
+        lblSigniture.Visible = False
+
+        ' Enables the exit button after the run button is pressed.
+        btnExit.Enabled = True
+
+        intAge = txtAge.Text
+        intDay = txtDayBorn.Text
+        intMonth = txtMonthBorn.Text
+
+        total = Math.Round((intAge + intDay) * intMonth / 2 - 1)
+
+        ' This stops the user from getting a result that is out of the range of the list index and stops the program before it errors.
+        If total >= 648 Then
+            MsgBox("You are a pokemon that is outside the built in pokemon, please come back when the program is updated with more pokemon.", 1)
+            End
+        End If
+
+        ' Showing the Pokemon name label to the user and determining what pokemon name to give the user.
+        lblPokemon.Visible = True
+        Dim labelText As String = "Your pokemon is " & pokemon(total)
+        lblPokemon.Text = labelText
+
+        ' Shows the PokeDex Number label to the user and sets the Label text to "PokeDex Numer ###"
+        lblPokeDex.Visible = True
+        Dim dexText As String = "Pokedex Number " & total + 1
+        lblPokeDex.Text = dexText
+
+        Dim resources As Object = My.Resources.ResourceManager
+
+        ' Sets the pokemon name to all lowercase to match the resource photo names.
+        Dim photo As String = pokemon(total).ToLower
+
+        ' Grabs a photo from the resource that has the same name as the pokemon and displays it in the photobox.
+        picPokemon.Image = resources.GetObject(photo)
+        picPokemon.Refresh()
+        picPokemon.Visible = True
+
+        ' Enables the Web button.
+        btnWeb.Enabled = True
+
+    End Sub
+
+    ' Exits the program
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    ' Resets the program to default, ready for another input.
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblPokemon.Visible = False
+        txtAge.Clear()
+        txtDayBorn.Clear()
+        txtMonthBorn.Clear()
+        btnExit.Enabled = False
+        txtAge.Focus()
+        picPokemon.Visible = False
+        lblPokeDex.Visible = False
+        btnWeb.Enabled = False
+    End Sub
+
+    ' Stops the user from entering non numeric data into the TextBox for age.
+    Private Sub txtAge_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs)
+
+        If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+
+    End Sub
+
+    ' Stops the user from entering non numeric data into the TextBox for day born.
+    Private Sub txtDayBorn_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs)
+
+        If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+
+    End Sub
+
+    ' Stops the user from entering non numeric data into the TextBox for month born.
+    Private Sub txtMonthBorn_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs)
+
+        If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+
+    End Sub
+
+    ' Opens The webpage for the users pokemon info in the default browser.
+    Private Sub btnWeb_Click(sender As Object, e As EventArgs) Handles btnWeb.Click
+        Process.Start("https://pokemongopokedex.site/" & pokemon(total).ToLower & "/")
+    End Sub
+End Class
